48ea164dafb732a72e03aaea2e1f94d4b2f17e1d
cxwu28 <719175288@qq.com>
2026-04-14 19:51:45 +0800

    fix

--- testnull.c
+++ testnull.c
@@ -7,8 +7,5 @@ int main() {
     printf(1, "Attempting to read from NULL (0x0)\n");
     int val = *p;  // reference memory
     printf(1, "Read value %d from NULL (unexpected)\n", val);
-    printf(1, "Attempting to write to NULL (0x0)\n");
-    *p = 42;
-    printf(1, "ERROR: write to NULL succeeded, p[0] = %d\n", *p);
     exit();
 }
